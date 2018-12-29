fc1793f8a92682c51b872c20f905aa1b40f92bbd
411496621 <411496621@qq.com>
2018-12-29 09:14:21 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Footer from './componnts/Footer/Footer'
+  export default {
+    components:{
+      Footer
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
